540ad0fc947c0d8f38b083e45a2cde8a83a61f8f
unknown <git.demont@gmail.com>
2024-10-28 10:29:18 +0100

    [enh] add fifo_resize and queue_resize functions to allow resizing + auto expand queue instead of triggering error when full

--- inst/include/fifo.hpp
+++ inst/include/fifo.hpp
@@ -41,7 +41,7 @@ using namespace Rcpp;
 //' @details Q[0] will be set to 0 and Q[Q.size() - 1] to 1.
 //' @return it returns nothing but Q will be modified in-place.
 // [[Rcpp::export(rng = false)]]
-void fifo_clear (Rcpp::IntegerVector Q) {
+void fifo_clear (Rcpp::IntegerVector &Q) {
   Q[0] = 0;
   Q[Q.size() - 1] = 1;
 }
@@ -63,6 +63,29 @@ Rcpp::IntegerVector fifo_create (const R_len_t size = 1,
   return out;
 }
 
+//' @title Resize FIFO Queue
+//' @name fifo_resize
+//' @description
+//' This function expand a fifo queue by doubling its capacity.
+//' @param Q, an IntegerVector; the fifo queue.
+//' @param value, an int; the value used to fill the queue. Default is NA_INTEGER.
+//' @details Q capacity will be doubled and all values copied and current starting position, Q[Q.size()], reset to 1.
+//' @return it returns nothing but Q will be modified in-place.
+// [[Rcpp::export(rng = false)]]
+void fifo_resize (Rcpp::IntegerVector &Q,
+                  const int value = NA_INTEGER) {
+  R_len_t SIZE = Q.size() > 3 ? 4 + 2 * (Q.size() - 3) : 4;
+  if(SIZE > (std::pow(2.0,31.0) - 3)) Rcpp::stop("fifo_resize: can't resize queue");
+  Rcpp::IntegerVector QQ = Rcpp::no_init_vector(SIZE);
+  QQ[0] = Q[0];
+  R_len_t j = 1;
+  for(R_len_t i = Q[Q.size() - 1]; i < Q.size() - 1; i++) QQ[j++] = Q[i];
+  for(R_len_t i = 1; i < Q[Q.size() - 1]; i++) QQ[j++] = Q[i];
+  for(R_len_t i = Q.size(); i < QQ.size(); i++) QQ[i] = value;
+  QQ[QQ.size() - 1] = 1;
+  Q = QQ;
+}
+
 //' @title Pop FIFO Queue
 //' @name fifo_pop
 //' @description
@@ -75,7 +98,7 @@ Rcpp::IntegerVector fifo_create (const R_len_t size = 1,
 //' Q[0] will be decreased by 1 and Q[Q.size() - 1] will be shift by -1
 //' @return an int, the 1st element of the queue.
 // [[Rcpp::export(rng = false)]]
-int fifo_pop (Rcpp::IntegerVector Q) {
+int fifo_pop (Rcpp::IntegerVector &Q) {
   R_len_t xx = Q.size() - 1;
   R_len_t n = Q[0];            // count
   R_len_t s = Q[xx];           // start index
@@ -103,7 +126,7 @@ int fifo_pop (Rcpp::IntegerVector Q) {
 //' Q[0] will be increased by 1 and Q[Q.size() - 1] will be shift by +1
 //' @return it returns nothing but Q will be modified in-place.
 // [[Rcpp::export(rng = false)]]
-void fifo_add (Rcpp::IntegerVector Q,
+void fifo_add (Rcpp::IntegerVector &Q,
                const int value) {
   R_len_t xx = Q.size() - 1;
   R_len_t n = Q[0];            // count
@@ -111,8 +134,10 @@ void fifo_add (Rcpp::IntegerVector Q,
   if((n < (xx - 1)) && (n >= 0)) {
     Q[0]++;
     Q[((s + n) < xx) ? (s + n) : (s + n - xx + 1)] = value;
-  } else { // should never happen
-    Rcpp::stop("fifo_add: can't add value");
+  } else { // dynamically expand queue
+    if(n < 0) Rcpp::stop("fifo_add: can't add value");
+    fifo_resize(Q);
+    fifo_add(Q, value);
   }
 }
 
@@ -131,6 +156,23 @@ Rcpp::IntegerVector queue_create (const R_len_t size = 1) {
   return out;
 }
 
+//' @title Resize Queue
+//' @name queue_resize
+//' @description
+//' This function expand a queue by doubling its capacity.
+//' @param Q, an IntegerVector; the queue.
+//' @param value, an int; the value used to fill the queue. Default is NA_INTEGER.
+//' @details Q capacity will be doubled and all values copied and current starting position, Q[Q.size()], reset to 1.
+//' @return it returns nothing but Q will be modified in-place.
+// [[Rcpp::export(rng = false)]]
+void queue_resize (Rcpp::IntegerVector &Q) {
+   R_len_t SIZE = Q.size() > 2 ? 3 + 2 * (Q.size() - 2) : 3;
+   if(SIZE > (std::pow(2.0,31.0) - 2)) Rcpp::stop("queue_resize: can't resize queue");
+   Rcpp::IntegerVector QQ = Rcpp::no_init_vector(SIZE);
+   std::copy(Q.begin(), Q.end(), QQ.begin());
+   Q = QQ;
+ }
+
 //' @title Pop Queue
 //' @name queue_pop
 //' @description
@@ -141,7 +183,7 @@ Rcpp::IntegerVector queue_create (const R_len_t size = 1) {
 //' On pop, Q[0] will be decreased by 1 while last entered element will be returned.
 //' @return an int, the most recently pushed element of the vector queue.
 // [[Rcpp::export(rng = false)]]
-int queue_pop (Rcpp::IntegerVector Q) {
+int queue_pop (Rcpp::IntegerVector &Q) {
   R_len_t n = Q[0];
   if(n > 0) {
     Q[0]--;
@@ -162,12 +204,14 @@ int queue_pop (Rcpp::IntegerVector Q) {
 //' On push, Q[0] will be increased by 1 while value will be positioned in Q at new Q[0].
 //' @return it returns nothing but Q will be modified in-place.
 // [[Rcpp::export(rng = false)]]
-void queue_push (Rcpp::IntegerVector Q, const int value) {
+void queue_push (Rcpp::IntegerVector &Q, const int value) {
   if((Q[0] < (Q.size() - 1)) && (Q[0] >= 0)) {
     Q[0]++;
     Q[Q[0]] = value;
-  } else { // should never happen
-    Rcpp::stop("queue_push: can't push value");
+  } else { // dynamically expand queue
+    if(Q[0] < 0) Rcpp::stop("queue_push: can't push value");
+    queue_resize(Q);
+    queue_push(Q, value);
   }
 }
 
